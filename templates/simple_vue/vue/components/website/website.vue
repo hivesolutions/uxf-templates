f0c7aff6ac908eadcad411caba84b9fdc0e37cfd
João Magalhães <joamag@gmail.com>
2019-03-21 09:14:06 +0000

    Small template change

--- templates/simple_vue/vue/components/website/website.vue
+++ templates/simple_vue/vue/components/website/website.vue
@@ -1,17 +1,19 @@
 <template>
-<div>
-    <ux-button v-on:click="onClick">Button</ux-button>
-    <ux-button color="green" v-on:click="onClick('green')">Button Green</ux-button>
-    <ux-button color="red" v-on:click="onClick('red')">Button Red</ux-button>
-    <ux-drop-down v-bind:values="items" v-bind:persist="true"
-                  v-on:value_change="onValueChange"></ux-drop-down>
-    <ux-text-field original-value="Put some text"></ux-text-field>
-    <ux-text-field type="date" original-value="Put a date"></ux-text-field>
-</div>
+    <div>
+        <ux-button v-on:click="onClick">Button</ux-button>
+        <ux-button color="green" v-on:click="onClick('green')">Button Green</ux-button>
+        <ux-button color="red" v-on:click="onClick('red')">Button Red</ux-button>
+        <ux-drop-down
+            v-bind:values="items"
+            v-bind:persist="true"
+            v-on:value_change="onValueChange"
+        ></ux-drop-down>
+        <ux-text-field original-value="Put some text"></ux-text-field>
+        <ux-text-field type="date" original-value="Put a date"></ux-text-field>
+    </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
 
 <script>
 import Vue from "vue";
